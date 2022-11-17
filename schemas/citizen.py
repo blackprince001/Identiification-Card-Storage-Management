0d6a09c8b90ad8e8ba56a38d5b9333756ecbb50c
Blackprincex <appiahboaduprince@gmail.com>
2022-11-17 05:25:15 +0000

    chore: add auto generated card_number attribute for citizens

--- schemas/citizen.py
+++ schemas/citizen.py
@@ -13,16 +13,31 @@ class CitizenCreate(CitizenBase):
     age: int
     sex: str
 
-    social_security_number: str
+    social_security_number: str = None
     date_of_birth: datetime
     place_of_birth: str
     card_number: str
 
     is_registered_voter: bool
 
-    # TODO validate the card_number of a Citizen
-    def _card_validator(self):
-        pass
+    def __init__(self, **data):
+        # generates card number for every instance
+        name = data["surnames"]
+        uniq = (
+            hash(
+                data["date_of_birth"].day
+                + (
+                    len(data["surnames"])
+                    * len(data["surnames"])
+                    / data["date_of_birth"].year
+                    + data["date_of_birth"].month
+                )
+            )
+            % 30_000_000
+        )
+
+        data["card_number"] = f"GHA-{uniq}-{name[0:3].upper()}"
+        super().__init__(**data)
 
 
 class Citizen(CitizenBase):
