e2b82a1e2507ebf3461db8b30fdb2e9888cfb0e7
Blackprincex <appiahboaduprince@gmail.com>
2022-11-18 22:18:00 +0000

    upd: finished `conftest` for pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import datetime
 import pytest
 
 from sqlalchemy import create_engine
@@ -10,4 +11,38 @@ Base = declarative_base()
 
 @pytest.fixture(scope="session")
 def engine():
-    return create_engine(url="sqlite+pysqlite:///database.db", future=True)
+    return create_engine(url="sqlite+pysqlite:///:memory:", future=True)
+
+
+@pytest.fixture(scope="session")
+def citizen():
+    doB = datetime.datetime.now()
+    return CitizenSchema(
+        firstnames="Prince",
+        surnames="Appiah",
+        age=99,
+        sex="M",
+        date_of_birth=doB,
+        place_of_birth="Amakom Kumasi",
+        is_registered_voter=False,
+    )
+
+
+@pytest.fixture(scope="session")
+def immigrant():
+    doB = datetime.datetime.now()
+    return ImmigrantSchema(
+        firstnames="Black",
+        surnames="Prince",
+        age=99,
+        sex="F",
+        date_of_birth=doB,
+        nationality="Pirate",
+    )
+
+
+@pytest.fixture
+def db(engine):
+    with Session(engine) as session:
+        Base.metadata.create_all(bind=engine)
+        yield session
