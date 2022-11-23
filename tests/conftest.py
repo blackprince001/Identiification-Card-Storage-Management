c42bfdbb3f0ae125cc6657a17d63e23a503d548f
Blackprincex <appiahboaduprince@gmail.com>
2022-11-23 21:26:24 +0000

    fix: resolved imports - Tests still failing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,15 @@ import datetime
 import pytest
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy.orm import Session
 from schemas.citizen import CitizenCreate as CitizenSchema
 from schemas.immigrant import ImmigrantCreate as ImmigrantSchema
-
-Base = declarative_base()
+from database.models import Base
 
 
 @pytest.fixture(scope="session")
 def engine():
-    return create_engine(url="sqlite+pysqlite:///:memory:", future=True)
+    return create_engine(url="sqlite+pysqlite:///database.db", future=True)
 
 
 @pytest.fixture(scope="session")
